6d228c5e71c911427de52b106426529495d85328
Achref <medachrefg@gmail.com>
2022-03-07 21:18:27 +0100

    adding axios to actionContext to make the code more easy

--- src/context/github/GitAction.js
+++ src/context/github/GitAction.js
@@ -1,48 +1,19 @@
+import axios from 'axios';
+
 const GITHUB_URL = process.env.REACT_APP_GIT_URL;
 const GITHUB_TOKEN = process.env.REACT_APP_gitToken;
 
+const urlAxios = axios.create({
+	baseURL: GITHUB_URL,
+	headers: {
+		Authorization: `token ${GITHUB_TOKEN}`,
+	},
+});
 //Get Search Results
 export const searchUsers = async (text) => {
 	const params = new URLSearchParams({
 		q: text,
 	});
-	const res = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-		headers: {
-			Authorization: `token ${GITHUB_TOKEN}`,
-		},
-	});
-
-	const { items } = await res.json();
-	return items;
-};
-//Get One User
-export const searchOneUser = async (login) => {
-	const res = await fetch(`${GITHUB_URL}/users/${login}`, {
-		headers: {
-			Authorization: `token ${GITHUB_TOKEN}`,
-		},
-	});
-
-	if (res.status === 404) {
-		window.location = '/notfound';
-	} else {
-		const data = await res.json();
-		return data;
-	}
-};
-// Get user repos
-export const getUserRepos = async (login) => {
-	const params = new URLSearchParams({
-		sort: 'created',
-		per_page: 10,
-	});
-
-	const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
-		headers: {
-			Authorization: `token ${GITHUB_TOKEN}`,
-		},
-	});
-
-	const data = await response.json();
-	return data;
+	const resp = await urlAxios.get(`/search/users?${params}`);
+	return resp.data.items;
 };
